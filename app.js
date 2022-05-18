29b3da9be8a12d4a5c470c720597a04fd0eeda66
Camden Withrow <cmwithrow@anderson.edu>
2022-05-18 00:57:12 -0400

    parse and format budget items and totals

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const worth = document.getElementById("net-worth");
 const budget = document.getElementById("budget");
 const transactions = document.getElementById("transactions");
 
+function formatAsDollar(num) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(num);
+}
+
 function getNetWorth(values) {
   let total = 0;
   Object.keys(values).forEach((key) => {
@@ -17,16 +24,53 @@ function getNetWorth(values) {
     }
   });
 
-  const totalInDollars = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
-
-
-  worth.innerHTML = totalInDollars.format(total);;
+  worth.innerHTML = formatAsDollar(total);
 }
 
-function getBudgets(categories) {}
+function getBudgets(categories) {
+  Object.keys(categories).forEach((key) => {
+    const id = categories[key]["id"];
+    const name = key;
+    const budgetedNum = categories[key]["budgeted"];
+    const actualNum = categories[key]["actual"];
+
+    const item = document.createDocumentFragment();
+
+    const budgetItemContainer = document.createElement("div");
+    const budgetItem = document.createElement("div");
+    const button = document.createElement("button");
+    const icon = document.createElement("div");
+    const category = document.createElement("p");
+    const budgetAmt = document.createElement("p");
+    const actualAmt = document.createElement("p");
+
+    item.appendChild(budgetItemContainer);
+
+    budgetItemContainer.appendChild(budgetItem);
+
+    budgetItem.appendChild(button);
+    budgetItem.appendChild(category);
+    budgetItem.appendChild(budgetAmt);
+    budgetItem.appendChild(actualAmt);
+
+    button.appendChild(icon);
+
+    budgetItemContainer.setAttribute("id", id);
+    budgetItemContainer.classList.add("budget-item-container");
+    budgetItem.classList.add("budget-item");
+    button.classList.add("expand-budget");
+    icon.classList.add("arrow", "right");
+    category.classList.add("category");
+    budgetAmt.classList.add("budget-amt");
+    actualAmt.classList.add("actual-amt");
+
+    category.innerText = name;
+    actualAmt.innerText = formatAsDollar(actualNum);
+    budgetAmt.innerText = formatAsDollar(budgetedNum);
+
+    budget.appendChild(item);
+  });
+}
 
 function getTransactions(transactions) {}
 
